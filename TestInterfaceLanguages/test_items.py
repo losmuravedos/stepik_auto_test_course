b3bc327da28b92bdb97a74088f92ea9e8d08f73c
thony <thony.pythony@gmail.com>
2023-10-27 14:43:10 +0400

    Rename function and add assert in test_items.py

--- TestInterfaceLanguages/test_items.py
+++ TestInterfaceLanguages/test_items.py
@@ -4,6 +4,6 @@ from selenium.webdriver.common.by import By
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_items(browser):
+def test_add_to_basket_button(browser):
     browser.get(link)
-    browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
+    assert browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
